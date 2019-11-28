88855d2a1ed5ee61bcb6698c5a5d7910fe522120
Jakob Buchert <jakob.buchert@gmail.com>
2019-11-28 22:10:43 +0100

    Create Random.ino

---
+++ Random/Random.ino
@@ -0,0 +1,78 @@
+//Random MiniFrank
+//random modul for MiniFrank, outputs 2 different random note Values, between -5V - +5V.
+//Speed depends the the potentiometer or clock signal input
+
+#include <SPI.h>
+#include <Wire.h>
+
+#define POT A1      //Potentiometer pin
+#define CLK_IN 0   // Clock interrupt
+
+//SPI
+#define DAC_1  A2   //CS DAC_1
+#define MOSI  4
+#define SCLK  6
+
+long timer = 0;
+bool trigger = 0;
+
+void setup() {
+
+  SPI.begin();  //Initialize SPI
+
+  //Setup Pins
+  pinMode(POT, INPUT);
+  pinMode(DAC_1, OUTPUT);
+
+  //new random seed from current potentiometer value
+  randomSeed(analogRead(POT));
+
+  //Clock interrupt
+  attachInterrupt(CLK_IN, clkInterrupt, RISING);
+}
+
+void loop() {
+
+  long potVal = analogRead(POT) ;     //read potentiometer value
+  long wait =  potVal * potVal >> 9;  //non linear wait from potentiometer value
+
+  if (wait < 1950) {   //disable auto trigger, if potentiometer value is under 1950
+
+    if (millis() - timer > wait) trigger = 1; //test timer reached
+  }
+
+  //send new random Signal
+  if (trigger) {
+    timer = millis();
+
+    setVoltage(DAC_1, 0, random(4096)); //random output 1
+    setVoltage(DAC_1, 1, random(4096)); //random output 2
+    trigger = 0; //reset trigger
+  }
+}
+
+//Send voltage value to MCP4822   //from https://github.com/elkayem/midi2cv
+
+void setVoltage(uint8_t dacpin, bool channel, unsigned int mV)  //select dac, select dac channel, mv Value
+{
+  mV /=2; //compensate 2x gain from dac
+
+  //16bit register
+  // |channel|-|gain(0)|shutdown(1)|....12bit Value....|
+
+  //build command
+  unsigned int command = channel ? 0x9000 : 0x1000;  //select channel 0 or 1 and activate DAC
+  command |= (mV & 0x0FFF);                          //set 12bit value
+
+  SPI.beginTransaction(SPISettings(24000000, MSBFIRST, SPI_MODE0));  //test SPI max Speed.. todo
+  digitalWrite(dacpin, LOW);    //activate transfer
+  SPI.transfer(command >> 8);   //send first byte..
+  SPI.transfer(command & 0xFF); //.. and the second
+  digitalWrite(dacpin, HIGH);   //disable transfer
+  SPI.endTransaction();         //end SPI transaction
+}
+
+//clock signal
+void clkInterrupt() {
+  trigger = 1;
+}
